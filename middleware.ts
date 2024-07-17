749448bb91d51b0987dc90e85c65011570c56a01
Sheikh Mohd Shoaib <116951006+shoaib-1457@users.noreply.github.com>
2024-07-17 14:13:38 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,8 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
-
-export default clerkMiddleware();
+import { authMiddleware } from "@clerk/nextjs/server";
+export default authMiddleware({
+  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
