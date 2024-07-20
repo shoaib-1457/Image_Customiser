151555335260df22ee938241b1bcba9d0c5ad892
shoaib-1457 <sheikh.mohd.shoaib1457@gmail.com>
2024-07-20 14:11:16 +0530

    commit

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,30 @@
+import Header from "@/components/shared/Header";
 import React from "react";
+import { transformationTypes } from "@/constants";
+import TransformationForm from "@/components/shared/TransformationForm";
+import { auth } from "@clerk/nextjs/server";
+import { getUserById } from "@/lib/actions/user.actions";
+import { redirect } from "next/dist/server/api-utils";
 
-const AddTransformationPage = () => {
-  return <div>AddTransformationPage</div>;
+const AddTransformationPage = async ({
+  params: { type },
+}: SearchParamProps) => {
+  const { userId } = auth();
+  const transformation = transformationTypes[type];
+  if (!userId) redirect("/sign-in");
+  const user = await getUserById(userId);
+  return (
+    <>
+      {" "}
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
+      <TransformationForm
+        action="Add"
+        userId={user._id}
+        type={transformation.type as TransformationTypeKey}
+        creditBalance={user.creditBalance}
+      />
+    </>
+  );
 };
 
 export default AddTransformationPage;
