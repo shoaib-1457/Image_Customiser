a871ae98881ae7fe8e44731333d51c44cf467c52
Sheikh Mohd Shoaib <116951006+shoaib-1457@users.noreply.github.com>
2024-07-20 14:39:39 +0530

    Update page.tsx

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,30 +1,34 @@
 import Header from "@/components/shared/Header";
-import React from "react";
-import { transformationTypes } from "@/constants";
 import TransformationForm from "@/components/shared/TransformationForm";
-import { auth } from "@clerk/nextjs/server";
+import { transformationTypes } from "@/constants";
 import { getUserById } from "@/lib/actions/user.actions";
-import { redirect } from "next/dist/server/api-utils";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const AddTransformationPage = async ({
+const AddTransformationTypePage = async ({
   params: { type },
 }: SearchParamProps) => {
   const { userId } = auth();
   const transformation = transformationTypes[type];
+
   if (!userId) redirect("/sign-in");
+
   const user = await getUserById(userId);
+
   return (
     <>
-      {" "}
       <Header title={transformation.title} subtitle={transformation.subTitle} />
-      <TransformationForm
-        action="Add"
-        userId={user._id}
-        type={transformation.type as TransformationTypeKey}
-        creditBalance={user.creditBalance}
-      />
+
+      <section className="mt-10">
+        <TransformationForm
+          action="Add"
+          userId={user._id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
     </>
   );
 };
 
-export default AddTransformationPage;
+export default AddTransformationTypePage;
